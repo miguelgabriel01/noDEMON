da4bc98c2f6ffd2d281b0b865b325624281a5895
Miguel Gabriel(supremo poder) <49694646+miguelgabriel01@users.noreply.github.com>
2019-09-23 23:46:59 -0300

    Delete adicionar.php

--- php/adicionar.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$puxar = $_POST['nome'] . "," . $_POST['mail'] . "," . $_POST['senha'].",".$_POST['senhac']."/n";
-	$escrever = fopen("arquivo.csv","a");
-	fwrite($escrever, $puxar);
-	header("location:login.php");
- ?>
