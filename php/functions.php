b257b51946d4d257182ba3b6060cce82f722dc96
Miguel Gabriel(supremo poder) <49694646+miguelgabriel01@users.noreply.github.com>
2019-09-23 23:48:26 -0300

    Delete functions.php

--- php/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
- 		$humano = "\n" . $_POST['nome'] . ',' . $_POST['email'] . ',' . $_POST['cidade'] $_POST['bairro'] . ',' . $_POST['tel'] . ',' . $_POST['senha'] $_POST['senha'];
-    $user = fopen('cadastro.csv', 'a');
-    fwrite($user, $humano);
-    header("location:cadastro.php"); 
-?>
