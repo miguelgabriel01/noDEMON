ea7a2dd1724b454957da6bbd2da6153a5a548a76
Miguel Gabriel(supremo poder) <49694646+miguelgabriel01@users.noreply.github.com>
2019-10-02 12:26:31 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>formulario</title>
+	<link rel="stylesheet" type="text/css" href="css/cadastro.css">
+</head>
+
+<body>
+<header>
+
+	<a href="">SNRC</a>
+	<nav>
+		<li><a href="index.php">Inicio</a></li>
+		<li><a href="sobre.php">Sobre</a></li>
+		<li><a href="#">Mapa</a></li>
+		<li><a href="escolas.php">Escolas</a></li>
+		<li><a href="#">cadastro</a></li>
+
+	</nav>
+</header>
+
+ <section class="formulario" >
+ 
+ <div class="cadastro"><a href="cadastroUsuario.php" >usuário</a></div>
+ <div class=" cadastro"><a href="usuariospagos.php" >escola</a></div>
+ 
+ </section>
+
+</body>
+</html>
