b875c23b5ad6bce942768c891cc08cb1f7fb9d24
Miguel Gabriel(supremo poder) <49694646+miguelgabriel01@users.noreply.github.com>
2019-10-04 10:23:11 -0300

    Delete addescola.php

--- php/addescola.php
+++
@@ -1,8 +0,0 @@
-<?php
-
- $dado = $_POST['escola'] . ',' . $_POST['email'] . ',' . $_POST['endereco'] . ',' . $_POST['cidade'] . ',' . $_POST['estado'] .  ',' . $_POST['diretor'] .  ',' . $_POST['Nalunos'] . ',' . $_POST['Nprofessores'] . ',' . $_POST['Nsalas'] . ',' . $_POST['senha'] . ',' . $_POST['senhaconfirmada'] . "\n";
-
-    $handle = fopen('escolas.csv', 'a');
-    fwrite($handle, $dado); 
-    header("location:/index.php");
-?>
