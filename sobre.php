71a409ff359b498a86ae1f2795f1eaec7d6ad4e5
brunomanoel2019 <49694126+brunomanoel2019@users.noreply.github.com>
2019-10-15 23:43:30 -0300

    Update sobre.php

--- sobre.php
+++ sobre.php
@@ -5,10 +5,8 @@
 <title>sobre</title>
 <link rel="stylesheet" type="text/css" href="css/sobre.css">
 </head>
-<body>
-	
+<body>	
 <header>
-
 	<a href="">SNRC</a>
 	<nav>
 		<li><a href="index.php">Inicio</a></li>
