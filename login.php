687ed89067fdfd0f62ffbb1dc16128b2ca2d939f
Miguel Gabriel(supremo poder) <49694646+miguelgabriel01@users.noreply.github.com>
2019-10-20 17:01:42 -0300

    Add files via upload
    Login para cadastro de escolas

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>login</title>
+<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>	
+<header>
+	<a href="">SNRC</a>
+	<nav>
+		<li><a href="index.php">Inicio</a></li>
+		<li><a href="sobre.php">Sobre</a></li>
+		<li><a href="mapaEscolaSCED.php">Mapa</a></li>
+		<li><a href="escolas.php">Escolas</a></li>
+		<li><a href="cadastroUsuario.php">Cadastro</a></li>
+
+	</nav>
+</header>
+
+<section class="imgP">
+	
+</section>
+
+
+</body>
+</html>
