b1b877908024dac5db524942911ebfe6c2ab1506
ThierryMatheus <49615769+ThierryMatheus@users.noreply.github.com>
2019-11-05 19:09:42 -0300

    Mudança do nome do user

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $conn = new PDO("mysql:host=localhost;dbname=id10860157_SNRC", "root", ""); 
+    $conn = new PDO("mysql:host=localhost;dbname=id10860157_SNRC", "id10860157_root", ""); 
 }
 catch(PDOException $e){
     echo "Erro do Banco de dados: " . $e->getMessage();
