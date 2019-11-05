567b39d811089f262fac8e1368c471a050b11830
ThierryMatheus <49615769+ThierryMatheus@users.noreply.github.com>
2019-11-05 19:08:57 -0300

    Mudança do nome do B.D

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $conn = new PDO("mysql:host=localhost;dbname=SNRC", "root", ""); 
+    $conn = new PDO("mysql:host=localhost;dbname=id10860157_SNRC", "root", ""); 
 }
 catch(PDOException $e){
     echo "Erro do Banco de dados: " . $e->getMessage();
