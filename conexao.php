f9b1e551e994fb7ae4346927df18da87ef5ed575
ThierryMatheus <49615769+ThierryMatheus@users.noreply.github.com>
2019-11-05 19:10:06 -0300

    Adição da password

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $conn = new PDO("mysql:host=localhost;dbname=id10860157_SNRC", "id10860157_root", ""); 
+    $conn = new PDO("mysql:host=localhost;dbname=id10860157_SNRC", "id10860157_root", "123456"); 
 }
 catch(PDOException $e){
     echo "Erro do Banco de dados: " . $e->getMessage();
