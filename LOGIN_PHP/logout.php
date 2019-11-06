3802845471466c4181e42c793291b93a2c3ba3de
brunomanoel2019 <49694126+brunomanoel2019@users.noreply.github.com>
2019-11-06 01:26:36 -0300

    Delete logout.php

--- LOGIN_PHP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	// Inicia a sessão
-    session_start();
-    // Destrói a sessão limpando todos os valores salvos
-    session_destroy(); 
-    // Redireciona o visitante
-    header("Location: ../index.php");
-exit; 
- ?>
