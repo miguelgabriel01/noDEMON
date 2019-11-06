376e4e265babd87b18cce0e7ce81ee7b25f15f05
brunomanoel2019 <49694126+brunomanoel2019@users.noreply.github.com>
2019-11-06 01:29:14 -0300

    Upload

---
+++ config/conexao.php
@@ -0,0 +1,11 @@
+<?php
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=SNRC", "root", ""); 
+}
+catch(PDOException $e){
+    echo "Erro do Banco de dados: " . $e->getMessage();
+}
+catch(Exception $e){
+    echo "Erro de aplicacao: " . $e->getMessage();
+}
+?>
