6f8312c3b2fa29d9131091f8e4abea2fb75e6f37
brunomanoel2019 <49694126+brunomanoel2019@users.noreply.github.com>
2019-11-06 01:24:55 -0300

    Delete addusuario.php

--- php/addusuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-
- $dado = $_POST['nome'] . ',' . $_POST['email'] . ',' . $_POST['endereco'] . ',' . $_POST['cidade'] . ',' . $_POST['estado'] .  ',' . $_POST['senha'] .  ',' . $_POST['senhaconfirmada'] . "\n";
-
-    $handle = fopen('usuarios.csv', 'a');
-    fwrite($handle, $dado); 
-    header("location:/index.php");
-?>
