88857d5edcf5f31c6bcdc124584cb05b75ef7b5d
ThierryMatheus <49615769+ThierryMatheus@users.noreply.github.com>
2019-12-04 16:55:02 -0300

    Conexao phpMyAdmin

--- config/conexao.php
+++ config/conexao.php
@@ -4,6 +4,8 @@
     public static function getConn() {
       if (self::$conn == null) {
           self::$conn = new PDO("mysql:host=localhost;dbname=SNRC", "root", "ifpe");
+        //conexao com phpMyAdmin
+         // self::$conn = new PDO("mysql:host=localhost;dbname=id11568186_snrc", "id11568186_root", "97116162");
       }
       return self::$conn;
     }
