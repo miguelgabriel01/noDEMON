8220853f51d1d35f0e201ac02f2e624f2e177481
Khevinn <karimoxiha@gmail.com>
2019-12-04 23:12:18 -0200

    Remoção do ínicio na nav e adicionando um href para a sigla SNRC

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 	
 ?>
 <header>
-	<a href="" style='padding-left: 20px;'>SNRC</a>
+	<a href="../index.php" style='padding-left: 20px;'>SNRC</a>
 	<nav>
-		<li><a href="../index.php">Inicio</a></li>
-		<li><a href="../view/cadastro_escola.php" >Cadastrar Escolas</a></li>
+<!-- 		<li><a href="../index.php">Inicio</a></li>
+ -->		<li><a href="../view/cadastro_escola.php" >Cadastrar Escolas</a></li>
 	<?php if(!isset($_SESSION["logado"])) : ?>
 		<li><a href="../view/cadastro_usuario.php">Cadastrar-se</a></li>
-		<li><a href="../view/login.php">Login</a></li>
+		<li><a href="../view/login.php">Entrar</a></li>
 	<?php endif ?>
 	<?php if(isset($_SESSION["logado"])) : ?>
 		<li><a><?=$_SESSION["logado"]["nome"]?></a></li>
