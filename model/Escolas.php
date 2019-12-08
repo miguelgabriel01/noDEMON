f8769c30314411bf1d4a029a2904abcb3d06052e
ThierryMatheus <thierrymatheusreal@gmail.com>
2019-12-08 16:11:37 -0300

    Retorno das coordenadas

--- model/Escolas.php
+++ model/Escolas.php
@@ -4,15 +4,17 @@
   class Escolas{
 
 
-  public function cadastrarEscolas($nome, $endereco, $cidade, $cep, $telefone, $mapa) {
+  public function cadastrarEscolas($nome, $endereco, $cidade, $cep, $telefone, $mapa, $lat, $lot) {
 		$con = Connection::getConn();
-		$consulta = $con->prepare("INSERT INTO escolas (nome, endereco, cidade, cep, telefone, mapa) VALUES (:nome, :endereco, :cidade, :cep, :telefone, :mapa)");
+		$consulta = $con->prepare("INSERT INTO escolas (nome, endereco, cidade, cep, telefone, mapa, lat, lot) VALUES (:nome, :endereco, :cidade, :cep, :telefone, :mapa, :lat, :lot)");
 		$consulta->bindValue(":nome", $nome);
 		$consulta->bindValue(":endereco", $endereco);
 		$consulta->bindValue(":cidade", $cidade);
 		$consulta->bindValue(":cep", $cep);
 		$consulta->bindValue(":telefone", $telefone);
 		$consulta->bindValue(":mapa", $mapa);
+		$consulta->bindValue(":lat", $lat);
+		$consulta->bindValue(":lot", $lot);
 		$consulta->execute();
 	}
 
