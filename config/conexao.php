6aaa8a1ff09fe4597026abc9eb7d575d754689e8
ThierryMatheus <thierrymatheusreal@gmail.com>
2019-12-08 16:07:03 -0300

    Controle de Latitude e Longitude

--- config/conexao.php
+++ config/conexao.php
@@ -3,7 +3,7 @@
     private static $conn;
     public static function getConn() {
       if (self::$conn == null) {
-          self::$conn = new PDO("mysql:host=localhost;dbname=SNRC", "root", "ifpe");
+          self::$conn = new PDO("mysql:host=localhost;dbname=SNRC", "root", "123");
         //conexao com phpMyAdmin
          // self::$conn = new PDO("mysql:host=localhost;dbname=id11568186_snrc", "id11568186_root", "97116162");
       }
