5f775501b31d7d3cbf52dd174aba8ee8c4d4f5d9
ThierryMatheus <thierrymatheusreal@gmail.com>
2019-12-08 16:08:47 -0300

    Controle de Latitude e Longitude

--- controller/cadastrar_escola.php
+++ controller/cadastrar_escola.php
@@ -8,14 +8,16 @@ $cidade = $_POST["cidade"];
 $cep = $_POST["cep"];
 $telefone = $_POST["telefone"];
 $mapa = $_POST["mapa"];
+$lat = $_POST["lat"];
+$lot = $_POST["lot"];
 
 $escola = new Escolas();
 
 
 
-if(!empty($nome) && !empty($endereco) && !empty($cidade) && !empty($cep) && !empty($telefone) && !empty($mapa)) {
+if(!empty($nome) && !empty($endereco) && !empty($cidade) && !empty($cep) && !empty($telefone) && !empty($mapa) && !empty($lat) && !empty($lot)) {
 
-	$escola->cadastrarEscolas($nome, $endereco, $cidade, $cep, $telefone, $mapa);
+	$escola->cadastrarEscolas($nome, $endereco, $cidade, $cep, $telefone, $mapa, $lat, $lot);
 
 		header("location: /index.php?msg=Escola cadastrada com sucesso!");
 }
